d3314ee559e79223fe04da407b069bd7a0149ae0
Rasujon3 <rasujon3@gmail.com>
2022-02-22 00:18:12 +0600

    33-2 Intro to JSON, JSON Structure, parse, stringify

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
+// javascript object Notation
+// JSON
 const user = { id: 11, name: "Gorib Amir", job: "actor" };
-console.log(user);
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+
+const shop = {
+  name: "Alia Store",
+  address: "Ranbir road",
+  profit: 15000,
+  products: ["laptop", "mobile", "pepsi"],
+  owner: {
+    name: "Alia Bhatt",
+    profession: "actor",
+  },
+  isExpensive: false,
+};
+
+const shopStringified = JSON.stringify(shop);
+// console.log(shop);
+console.log(shopStringified);
+const converted = JSON.parse(shopStringified);
+console.log(converted.owner);
