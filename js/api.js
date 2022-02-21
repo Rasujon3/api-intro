49ab8985144ffb0afd62480d5cde3cebe042d51f
Rasujon3 <rasujon3@gmail.com>
2022-02-22 00:48:40 +0600

    33-4 Load more data, more APIs, send data to function

--- js/api.js
+++ js/api.js
@@ -1,5 +1,18 @@
 function loadData() {
   fetch("https://jsonplaceholder.typicode.com/todos/1")
     .then((response) => response.json())
+    .then((data) => displayUsers(data));
+}
+function loadUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
     .then((json) => console.log(json));
+}
+function loadPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+function displayUsers(data) {
+  console.log(data);
 }
